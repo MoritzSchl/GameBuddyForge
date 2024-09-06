b22bec53324ada4a2bd327fcfdb95ae2e14d18ee
moritzschleimer <Schleimer.moritz@gmail.com>
2024-09-06 11:11:17 +0200

    init commit

---
+++ GameBuddyForge/GameBuddyForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameBuddyForgeApp.swift
+//  GameBuddyForge
+//
+//  Created by Moritz Schleimer on 06.09.24.
+//
+
+import SwiftUI
+
+@main
+struct GameBuddyForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
